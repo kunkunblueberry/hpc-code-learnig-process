a2a8563f4efa6c6ae4da85aa980ccb7f703dd13b
kunkun <72174834+kunkunblueberry@users.noreply.github.com>
2025-09-22 22:36:15 +0800

    Update time function.cpp

--- 多线程&现代cpp特性/time function.cpp
+++ 多线程&现代cpp特性/time function.cpp
@@ -7,3 +7,13 @@ auto t1 = std::chrono::steady_clock::now();
 auto dt = t1 - t0;
 int64_t ms = std::chrono::duration_cast<chrono::milliseconds>(dt).count();
 std::cout << "time elapsed:" << ms << "ms" << std::endl;
+
+
+
+std::this_thread::sleep_for
+std::this_thread::sleep_for(std::chrono::milliseconds(400));  //让线程休眠 400 毫秒，替代 Unix 专属的usleep
+sleep_for在this_thread命名空间下面哦，完整要这么写
+
+
+std::this_thread::sleep_until
+sleep_until(steady_clock::now() + milliseconds(400))
